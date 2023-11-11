247ade8483e27b97640827aa2019d0aa6944d895
Maksim Atenyaev <atenjaev@gmail.com>
2023-11-11 21:00:03 +0600

    add tests

--- src/test/java/pages/ResultSearchPage.java
+++ src/test/java/pages/ResultSearchPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -10,6 +11,7 @@ public class ResultSearchPage {
     SelenideElement foundProducts = $(".default-wrapper .search");
 
 
+    @Step("Проверка отображения найденного продукта")
     public void checkProduct(String value){
         foundProducts.shouldHave(Condition.text(value));
     }
